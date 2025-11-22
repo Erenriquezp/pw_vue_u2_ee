884fbca0e996db56c847a2e101730b1662435283
Edison Enriquez <erenriquezp@uce.edu.ec>
2025-11-22 00:42:49 +0000

    TALLER 13 Componentes View

---
+++ src/components/Contador.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Universidad Centrla del Ecuador</h1>
+    <h1>Carrera de Computacion</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: { // Nuevo puerto a desplegarse
+    client: {
+      webSocketURL: {
+        protocol: "wss",
+      },
+    },
+    port: 8081,
+    host: "0.0.0.0"
+  }
 })
